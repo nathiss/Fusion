451480974b6f3e5651fb2b9f4752aae54fd92207
Kamil Rusin <kamil.jakub.rusin@gmail.com>
2019-06-27 19:01:20 +0200

    Added signal handling.
    * Added threads joining.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,45 @@
+#include <functional>
+#include <iostream>
 #include <thread>
 #include <vector>
 
+#include <boost/asio.hpp>
+
 #include <fusion_server/server.hpp>
+
+void HandleSignal(boost::asio::io_context& ioc,
+  const boost::system::error_code& ec, int signal) noexcept {
+  if (ec) {
+    std::cerr << "HandleSignal: " << ec.message() << std::endl;
+    // We do nothing, since we want to exit.
+  }
+  std::cerr << "[Warning] Received signal: " << signal << ". Shutting down." << std::endl;
+  ioc.stop();
+}
 
 int main() {
   auto& server = fusion_server::Server::GetInstance();
   server.StartAccepting();
 
   auto& ioc = server.GetIOContext();
+
+  boost::system::error_code ec;
+  boost::asio::signal_set signals{ioc};
+  signals.add(SIGINT, ec);
+  if (ec) {
+    std::cerr << "Error: " << ec.message() << std::endl;
+    return EXIT_FAILURE;
+  }
+  signals.add(SIGTERM, ec);
+  if (ec) {
+    std::cerr << "Error: " << ec.message() << std::endl;
+    return EXIT_FAILURE;
+  }
+  signals.async_wait(
+    [&ioc] (const boost::system::error_code& ec, int signal) {
+      HandleSignal(ioc, ec, signal);
+    }
+  );
 
   std::vector<std::thread> workers;
   workers.reserve(std::thread::hardware_concurrency() - 1);
@@ -16,6 +48,9 @@ int main() {
     workers.emplace_back([&ioc]{ ioc.run(); });
 
   ioc.run();
+
+  for (auto& worker : workers)
+    worker.join();
 
   return EXIT_SUCCESS;
 }
