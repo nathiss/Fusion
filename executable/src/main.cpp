b02aadf1266b73caefef86027496be0aa68045ee
Kamil Rusin <kamil.jakub.rusin@gmail.com>
2019-07-11 20:52:51 +0200

    Created a logger factory.
    * Removed loggers creation from main.cpp.

--- executable/src/main.cpp
+++ executable/src/main.cpp
@@ -7,11 +7,10 @@
 #include <vector>
 
 #include <boost/asio.hpp>
-#include <spdlog/spdlog.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/sinks/basic_file_sink.h>
 
 #include <fusion_server/server.hpp>
+#include <fusion_server/system_abstractions.hpp>
 
 void HandleSignal(boost::asio::io_context& ioc,
   const boost::system::error_code& ec, int signal) noexcept {
@@ -25,97 +24,16 @@ void HandleSignal(boost::asio::io_context& ioc,
 }
 
 void InitLogger() noexcept {
-  auto pattern = std::string{"[%H:%M:%S:%e] [thread %t] [%^%l@%n%$] %v"};
-#ifdef DEBUG
-  auto console_log_level = spdlog::level::debug;
-#else
-  auto console_log_level = spdlog::level::warn;
-#endif
+  namespace fs = fusion_server::system_abstractions;
 
-
-  // All
+  // TODO: read the path from config file
   auto all_file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/all.log", true);
-  all_file_sink->set_pattern(pattern);
+  all_file_sink->set_pattern(fs::kLoggerPattern);
 
-  // Server
-  auto server_console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-  server_console_sink->set_level(console_log_level);
-  server_console_sink->set_pattern(pattern);
-
-  auto server_file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/server.log", true);
-  server_file_sink->set_level(spdlog::level::debug);
-  server_file_sink->set_pattern(pattern);
-
-  spdlog::sinks_init_list server_sink_list{server_console_sink, server_file_sink, all_file_sink};
-
-  auto server_logger = std::make_shared<spdlog::logger>("server", std::move(server_sink_list));
-  server_logger->set_level(spdlog::level::debug);
-  server_logger->flush_on(console_log_level);
-  spdlog::register_logger(server_logger);
-
-  // Listener
-  auto listener_console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-  listener_console_sink->set_level(console_log_level);
-  listener_console_sink->set_pattern(pattern);
-
-  auto listener_file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/listener.log", true);
-  listener_file_sink->set_level(spdlog::level::debug);
-  listener_file_sink->set_pattern(pattern);
-
-  spdlog::sinks_init_list listener_sink_list{listener_console_sink, listener_file_sink, all_file_sink};
-
-  auto listener_logger = std::make_shared<spdlog::logger>("listener", std::move(listener_sink_list));
-  listener_logger->set_level(spdlog::level::debug);
-  listener_logger->flush_on(console_log_level);
-  spdlog::register_logger(listener_logger);
-
-  // Game
-  auto game_console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-  game_console_sink->set_level(console_log_level);
-  game_console_sink->set_pattern(pattern);
-
-  auto game_file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/game.log", true);
-  game_file_sink->set_level(spdlog::level::debug);
-  game_file_sink->set_pattern(pattern);
-
-  spdlog::sinks_init_list game_sink_list{game_console_sink, game_file_sink, all_file_sink};
-
-  auto game_logger = std::make_shared<spdlog::logger>("game", std::move(game_sink_list));
-  game_logger->set_level(spdlog::level::debug);
-  game_logger->flush_on(console_log_level);
-  spdlog::register_logger(game_logger);
-
-  // HTTP
-  auto http_console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-  http_console_sink->set_level(console_log_level);
-  http_console_sink->set_pattern(pattern);
-
-  auto http_file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/http.log", true);
-  http_file_sink->set_level(spdlog::level::debug);
-  http_file_sink->set_pattern(pattern);
-
-  spdlog::sinks_init_list http_sink_list{http_console_sink, http_file_sink, all_file_sink};
-
-  auto http_logger = std::make_shared<spdlog::logger>("http", std::move(http_sink_list));
-  http_logger->set_level(spdlog::level::debug);
-  http_logger->flush_on(console_log_level);
-  spdlog::register_logger(http_logger);
-
-  // WebSocket
-  auto websocket_console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-  websocket_console_sink->set_level(console_log_level);
-  websocket_console_sink->set_pattern(pattern);
-
-  auto websocket_file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/websocket.log", true);
-  websocket_file_sink->set_level(spdlog::level::debug);
-  websocket_file_sink->set_pattern(pattern);
-
-  spdlog::sinks_init_list websocket_sink_list{websocket_console_sink, websocket_file_sink, all_file_sink};
-
-  auto websocket_logger = std::make_shared<spdlog::logger>("websocket", std::move(websocket_sink_list));
-  websocket_logger->set_level(spdlog::level::debug);
-  websocket_logger->flush_on(console_log_level);
-  spdlog::register_logger(websocket_logger);
+  fs::CreateLogger("server", true, all_file_sink);
+  fs::CreateLogger("listener", true, all_file_sink);
+  fs::CreateLogger("http", true, all_file_sink);
+  fs::CreateLogger("websocket", true, all_file_sink);
 }
 
 int main() {
--- include/fusion_server/system_abstractions.hpp
+++ include/fusion_server/system_abstractions.hpp
@@ -15,6 +15,10 @@
 #include <string>
 #include <utility>
 
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
+
 #include <fusion_server/package_parser.hpp>
 
 namespace fusion_server {
@@ -56,6 +60,65 @@ template <typename... Args>
  *   The session connected to the client.
  */
 using IncommingPackageDelegate = std::function< void(const PackageParser::JSON&  package, WebSocketSession* session) >;
+
+static constexpr const char* kLoggerPattern = "[%H:%M:%S:%e] [thread %t] [%^%l@%n%$] %v";
+
+/**
+ * @brief Creates a logger.
+ * This function creates a logger and associates it with the given name.
+ * If @p register is set to true, the new logger will be registered in global
+ * registry.
+ *
+ * @tparam Sinks
+ *   A pack of types that meet spdlog::sink_ptr concepts.
+ *
+ * @param logger_name
+ *   The name of a new logger.
+ *
+ * @param register
+ *   If true the logger will be registered in the global registry.
+ *
+ * @param sinks
+ *   A pack of sinks that will be forwarded to the new logger object.
+ *
+ * @return
+ *   A pointer to the new logger.
+ *
+ * @see [spdlog Sinks](https://github.com/gabime/spdlog/wiki/4.-Sinks)
+ * @see [spdlog Logger registry](https://github.com/gabime/spdlog/wiki/5.-Logger-registry)
+ */
+template <typename ...Sinks>
+auto CreateLogger(std::string logger_name, bool registrate, Sinks... sinks) noexcept {
+
+#ifdef DEBUG
+  auto console_log_level = spdlog::level::debug;
+#else
+  auto console_log_level = spdlog::level::warn;
+#endif
+
+  // TODO: read the path from config file
+  std::string file_name{"log/"};
+  file_name += logger_name + ".log";
+
+  auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(file_name, true);
+  file_sink->set_level(console_log_level);
+  file_sink->set_pattern(kLoggerPattern);
+
+  auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+  console_sink->set_level(console_log_level);
+  console_sink->set_pattern(kLoggerPattern);
+
+  spdlog::sinks_init_list sinks_list{file_sink, console_sink, std::forward<Sinks>(sinks)...};
+
+  auto logger = std::make_shared<spdlog::logger>(logger_name, std::move(sinks_list));
+  logger->set_level(spdlog::level::debug);
+  logger->flush_on(console_log_level);
+
+  if (registrate)
+    spdlog::register_logger(logger);
+
+  return logger;
+}
 
 }  // namespace system_abstractions
 
