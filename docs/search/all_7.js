50bd85cb47e103f5257a8e132f5511722fb47c0d
Kamil Rusin <kamil.jakub.rusin@gmail.com>
2019-07-12 11:38:47 +0200

    Updated docs.

--- docs/search/all_7.js
+++ docs/search/all_7.js
@@ -5,6 +5,7 @@ var searchData=
   ['handleread',['HandleRead',['../df/d9c/classfusion__server_1_1HTTPSession.html#aa813ce90624987f38b45f46708078506',1,'fusion_server::HTTPSession::HandleRead()'],['../db/d0f/classfusion__server_1_1WebSocketSession.html#a77ae7b57464e7f31d9b7a3257e15867e',1,'fusion_server::WebSocketSession::HandleRead()']]],
   ['handlewrite',['HandleWrite',['../df/d9c/classfusion__server_1_1HTTPSession.html#af1926ae945a8fd047a707378d6b98590',1,'fusion_server::HTTPSession::HandleWrite()'],['../db/d0f/classfusion__server_1_1WebSocketSession.html#afb2eda707b5672090893bcd6562d870b',1,'fusion_server::WebSocketSession::HandleWrite()']]],
   ['handshake_5fcomplete_5f',['handshake_complete_',['../db/d0f/classfusion__server_1_1WebSocketSession.html#a749f83ec225befb23be6b2afb6569dc4',1,'fusion_server::WebSocketSession']]],
+  ['has_5fstopped_5f',['has_stopped_',['../d4/d1d/classfusion__server_1_1Server.html#a4624e6c278a6fe65c56d843a3608c970',1,'fusion_server::Server']]],
   ['health_5f',['health_',['../d5/d95/classfusion__server_1_1Player.html#aab1898183f5402deb163dbd07748ba7b',1,'fusion_server::Player']]],
   ['http_5fsession_2ecpp',['http_session.cpp',['../d7/db4/http__session_8cpp.html',1,'']]],
   ['http_5fsession_2ehpp',['http_session.hpp',['../df/df4/http__session_8hpp.html',1,'']]],
--- docs/search/functions_c.js
+++ docs/search/functions_c.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['server',['Server',['../d4/d1d/classfusion__server_1_1Server.html#a173ca05d64aba33c6afe1f069d312e07',1,'fusion_server::Server']]],
+  ['shutdown',['Shutdown',['../d4/d1d/classfusion__server_1_1Server.html#a6db7a3282d6b43ef7e313dcc36bc2c45',1,'fusion_server::Server']]],
   ['startaccepting',['StartAccepting',['../d4/d1d/classfusion__server_1_1Server.html#aecfcada9e5faf3165e959a4bd4aa06a5',1,'fusion_server::Server']]]
 ];
